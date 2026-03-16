b8c460e9ced13f225495288c4d73dc129d5ca6e5
Vladyslav Isov <s32819@pjwstk.edu.pl>
2026-03-17 01:15:00 +0300

    Add basic input handling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter numbers separated by space:");
+
+string input = Console.ReadLine();
+
+string[] parts = input.Split(" ");
+
+int[] numbers = new int[parts.Length];
+
+for (int i = 0; i < parts.Length; i++)
+{
+    numbers[i] = int.Parse(parts[i]);
+}
+
+Console.WriteLine("Numbers were successfully read.");
